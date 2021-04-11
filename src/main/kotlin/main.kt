7469224385d6562c031ba37ba52a10bb3fc0812e
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-04-11 08:08:54 +0300

    nested classes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,7 @@
-import generics.*
+import nestedClasses.Outer
 
 fun main(args: Array<String>) {
-    val box: Box<Int> = Box<Int>(1)
-    val box1 = Box(2).also {
-        println(box.value)
-        println(it.value)
-    }
-
-    println(box.value)
-    println(box1.value)
-
-    println(nextT())
-
-
+    println(Outer.Nested().foo())
+    println(Outer().Inner().foo())
 }
 
---
+++ src/main/kotlin/nestedClasses/Outer.kt
@@ -0,0 +1,12 @@
+package nestedClasses
+
+class Outer {
+    private val bar: Int = 1
+    class Nested {
+        fun foo() = 2
+    }
+
+    inner class Inner {
+        fun foo() = bar
+    }
+}
