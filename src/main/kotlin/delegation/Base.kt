8c8d7d3351ac61755f1b3e612cde3f0b1a7300c8
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-04-11 08:48:24 +0300

    delegation

---
+++ src/main/kotlin/delegation/Base.kt
@@ -0,0 +1,12 @@
+package delegation
+
+interface Base { fun print() }
+
+class BaseImpl(val x: Int) : Base {
+    override fun print() {
+        print(x)
+    }
+}
+
+class Derived(b: Base) : Base by b
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,8 @@
-import enumerations.IntArithmetics
-import enumerations.RGB
-import enumerations.printAllValues
-import nestedClasses.Outer
-import objects.adHoc
-
-
+import delegation.BaseImpl
+import delegation.Derived
 
 fun main(args: Array<String>) {
-    println(Outer.Nested().foo())
-    println(Outer().Inner().foo())
-
-    val a = 13
-    val b = 31
-    for (f in IntArithmetics.values()) {
-        println("$f($a, $b) = ${f.apply(a, b)}")
-        println("$f($a, $b) = ${f.applyAsInt(a, b)}")
-    }
-
-    printAllValues<RGB>()
-
-    print(adHoc.x + adHoc.y)
+    val b = BaseImpl(10)
+    Derived(b).print()
 }
 
